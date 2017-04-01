6f35f271ac4eb9f65a388feabe7d3daa78e3d0d9
Mehul Patel <mehulp715@gmail.com>
2017-04-01 16:29:51 -0400

    Add countdown timer

--- app/src/main/java/com/example/mehul/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/mehul/braintrainer/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mehul.braintrainer;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CountDownTimer timer = new CountDownTimer(10000, 1000){
+            TextView timerView = (TextView) findViewById(R.id.timerView);
+
+            public void onTick(long millisUntilFinished) {
+                timerView.setText(String.valueOf(millisUntilFinished / 1000));
+            }
+
+            public void onFinish() {
+                timerView.setText("0");
+            }
+
+        };
+
+        timer.start();
     }
 }
