861f6e834238b015bb91b562daf68ef68156c5e8
Mehul Patel <mehulp715@gmail.com>
2017-04-01 17:06:57 -0400

    Update Expression class variables and getValue method
    Refactored getValue to calculateValue and updated to calculate value on initialize
    getValue now returns the value
    Class variables are now private

--- app/src/main/java/com/example/mehul/braintrainer/Expression.java
+++ app/src/main/java/com/example/mehul/braintrainer/Expression.java
@@ -6,14 +6,16 @@ package com.example.mehul.braintrainer;
 
 public class Expression {
 
-    int leftOperand;
-    int rightOperand;
-    String operator;
+    private int leftOperand;
+    private int rightOperand;
+    private String operator;
+    private int value;
 
     public Expression(int leftOperand, int rightOperand, String operator){
         this.leftOperand = leftOperand;
         this.rightOperand = rightOperand;
         this.operator = operator;
+        this.value = this.calculateValue();
     }
 
     public String getExprString(){
@@ -21,6 +23,10 @@ public class Expression {
     }
 
     public int getValue(){
+        return this.value;
+    }
+
+    private int calculateValue(){
         int returnValue;
 
         switch(this.operator){
