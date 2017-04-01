87c180dc1d66a3bfcbc16d71792f0062daa711d9
Mehul Patel <mehulp715@gmail.com>
2017-04-01 16:42:42 -0400

    Generate random expression on start

--- app/src/main/java/com/example/mehul/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/mehul/braintrainer/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,7 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CountDownTimer timer = new CountDownTimer(10000, 1000){
+        TextView exprView = (TextView) findViewById(R.id.exprView);
+        Expression expr = getRandomExpression();
+        exprView.setText(expr.getExprString());
+
+        CountDownTimer timer = new CountDownTimer(5000, 1000){
             TextView timerView = (TextView) findViewById(R.id.timerView);
 
             public void onTick(long millisUntilFinished) {
@@ -26,5 +32,20 @@ public class MainActivity extends AppCompatActivity {
         };
 
         timer.start();
+    }
+
+    public int randInt(int min, int max) {
+        Random rand = new Random();
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+
+        return randomNum;
+    }
+
+    public Expression getRandomExpression(){
+        int leftOperand = randInt(0,100);
+        int rightOperand = randInt(0, 100);
+        String operator = "+";
+
+        return new Expression(leftOperand, rightOperand, operator);
     }
 }
